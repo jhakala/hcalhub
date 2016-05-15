f91efb6d4d306bc6791d32a67552810784f0e606
John Hakala <john_hakala@brown.edu>
2016-05-15 12:02:12 +0200

    gui submission of issue report

--- issues/urls.py
+++ issues/urls.py
@@ -6,4 +6,5 @@ app_name = 'issues'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^submit$', views.ReportView.as_view(), name='submit'),
 ]
--- issues/views.py
+++ issues/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
+from issues.models import Issue, Assignee
+
 
 # Create your views here.
 
@@ -18,3 +21,16 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Issue
     template_name = 'issues/detail.html'
+
+class ReportView(generic.CreateView):
+    model = Issue
+    fields = ['issue_label', 'issue_comment', 'issue_category', 'issue_assignee']
+    template_name = 'issues/submit.html'
+
+    def form_valid(self, form):
+        newIssue = form.save(commit=False)
+        newIssue.pub_date = timezone.now()
+        newIssue.save()
+        return HttpResponseRedirect(newIssue.id)
+
+#TODO make a ModifyView
